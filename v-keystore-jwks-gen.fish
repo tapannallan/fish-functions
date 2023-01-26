d63e853e9c29a01958deb8a301540e71143a991d
Tapan Chandra Nallan <4446423+tapannallan@users.noreply.github.com>
2023-01-26 02:32:01 +0100

    generate jwks for keystore entries

--- v-keystore-jwks-gen.fish
+++ v-keystore-jwks-gen.fish
@@ -10,11 +10,11 @@ function v-keystore-jwks-gen
     #access-token-keys
     keytool -list -storepass $STOREPASS -keystore $VERIMI_KEYSTORE | grep PrivateKeyEntry | grep access | cut -d',' -f1 | while read -la keyalias; 
         set -l algidentifier (echo $keyalias | cut -d'-' -f4)
-        java -jar $TOOLS_HOME/get-jwk.jar -alias $keyalias -jksPath $VERIMI_KEYSTORE -password $STOREPASS -alg $algidentifier -type private > "$KEYS_PATH/$keyalias.priv.json"
-        java -jar $TOOLS_HOME/get-jwk.jar -alias $keyalias -jksPath $VERIMI_KEYSTORE -password $STOREPASS -alg $algidentifier -type public > "$KEYS_PATH/$keyalias.pub.json"
+        java -jar $TOOLS_HOME/get-jwk.jar -alias $keyalias -jksPath $VERIMI_KEYSTORE -password $STOREPASS -alg $algidentifier -type private > "$KEYS_PATH/$keyalias.priv.jwk"
+        java -jar $TOOLS_HOME/get-jwk.jar -alias $keyalias -jksPath $VERIMI_KEYSTORE -password $STOREPASS -alg $algidentifier -type public > "$KEYS_PATH/$keyalias.pub.jwk"
     end
 
     #federation signing key
-    java -jar $TOOLS_HOME/get-jwk.jar -alias "federation-signing-key" -jksPath $VERIMI_KEYSTORE -password $STOREPASS -alg es256 -type private > "$KEYS_PATH/federation-signing-key-es256.priv.json"
-    java -jar $TOOLS_HOME/get-jwk.jar -alias "federation-signing-key" -jksPath $VERIMI_KEYSTORE -password $STOREPASS -alg es256 -type public > "$KEYS_PATH/federation-signing-key-es256.pub.json"
+    java -jar $TOOLS_HOME/get-jwk.jar -alias "federation-signing-key" -jksPath $VERIMI_KEYSTORE -password $STOREPASS -alg es256 -type private > "$KEYS_PATH/federation-signing-key-es256.priv.jwk"
+    java -jar $TOOLS_HOME/get-jwk.jar -alias "federation-signing-key" -jksPath $VERIMI_KEYSTORE -password $STOREPASS -alg es256 -type public > "$KEYS_PATH/federation-signing-key-es256.pub.jwk"
 end
